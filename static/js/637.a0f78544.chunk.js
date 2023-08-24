241593b946430197e00c88fd35eaea5443dbce3f
etopritika <67103761+etopritika@users.noreply.github.com>
2023-08-24 11:55:59 +0000

    Deploying to gh-pages from @ etopritika/team-project-7@9019741bf52dac09eab2bcc2d551d77fdb2391ad 🚀

---
+++ static/js/637.a0f78544.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_team_project=self.webpackChunkreact_team_project||[]).push([[637],{637:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsx)(n.Fragment,{})}}}]);
+//# sourceMappingURL=637.a0f78544.chunk.js.map
