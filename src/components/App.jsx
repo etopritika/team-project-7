2c02054d6f531abafd970e5f83457623161e8bf5
Nikolai-desig <rinzleramad79@gmail.com>
2023-08-25 22:04:15 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,35 +5,62 @@ import RestrictedRoute from './RestrictedRoute';
 // import Layout from './Layout';
 import { Suspense } from 'react';
 
-const MainPage = lazy(() => import('../pages/MainPage'));
-const RegisterPage = lazy(() => import('../pages/RegisterPage'));
-const LoginPage = lazy(() => import('../pages/LoginPage'));
-const AccountPage = lazy(() => import('../pages/AccountPage'));
+import routes from 'routes';
+import RegisterForm from './Forms/RegisterForm/RegisterForm';
+import LoginrForm from './Forms/LoginForm/LoginForm';
+import MainPage from 'pages/MainPage';
+import AccountPage from 'pages/AccountPage';
+import CalendarPage from 'pages/CalendarPage';
+import ChoosedMonth from './ChoosedMonth/ChoosedMonth';
+import ChoosedDay from './ChoosedDay/ChoosedDay';
+import StatisticsPage from 'pages/StatisticsPage';
 
 export const App = () => {
   return (
     <Suspense fallback={null}>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute redirectTo="/user" component={<RegisterPage />} />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute redirectTo="/user" component={<LoginPage />} />
-          }
-        />
-        <Route
-          path="/user"
-          element={
-            <PrivateRoute redirectTo="/login" component={<AccountPage />} />
-          }
-        />
+        <Route>
+          <Route element={<PubliceRourtes />}>
+            <Route path={routes.mainPage} element={MainPage}></Route>
+            <Route path={routes.registerPage} element={RegisterForm}></Route>
+            <Route path={routes.loginPage} element={LoginrForm}></Route>
+          </Route>
+          <Route element={<PrivateRoutes />}>
+            <Route path={routes.accountPage} element={AccountPage}></Route>
+            <Route path={routes.calendarPage} element={CalendarPage}>
+              <Route path={routes.calendarMonth} element={ChoosedMonth}></Route>
+              <Route path={routes.calendarDay} element={ChoosedDay}></Route>
+            </Route>
+            <Route path={routes.statisticsPage} element={StatisticsPage}></Route>
+          </Route>
+        </Route>
       </Routes>
     </Suspense>
   );
 };
+
+// return (
+//   <Suspense fallback={null}>
+//     <Routes>
+//       <Route path="/" element={<MainPage />} />
+//       <Route
+//         path="/register"
+//         element={
+//           <RestrictedRoute redirectTo="/user" component={<RegisterPage />} />
+//         }
+//       />
+//       <Route
+//         path="/login"
+//         element={
+//           <RestrictedRoute redirectTo="/user" component={<LoginPage />} />
+//         }
+//       />
+//       <Route
+//         path="/user"
+//         element={
+//           <PrivateRoute redirectTo="/login" component={<AccountPage />} />
+//         }
+//       />
+//     </Routes>
+//   </Suspense>
+// );
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const routes = {
+    mainPage: '/',
+    registerPage: '/register',
+    loginPage: '/login',
+    accountPage: '/account',
+    calendarPage: '/calendar',
+    calendarMonth: '/calendar/month/:currentDate',
+    calendarDay: '/calendar/day/:currentDate',
+    statisticsPage: '/statistics',
+  };
+  
+  export default routes;
