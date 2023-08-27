4b3086acc1c5276e640ca2dc4f0f4c48e78360d3
etopritika <dimaprytyka@gmail.com>
2023-08-27 16:36:14 +0300

    init slice changes

--- src/redux/Auth/authSlice.js
+++ src/redux/Auth/authSlice.js
@@ -10,24 +10,25 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {
-    [register.fulfilled](state, action) {
-      console.log('register-action.payload: ', action.payload);
-      //   state.user = action.payload.user;
-      //   state.token = action.payload.token;
-      //   state.isLoggedIn = true;
-    },
-    [logIn.fulfilled](state, action) {
-      console.log('login-action.payload: ', action.payload);
-      //   state.user = action.payload.user;
-      //   state.token = action.payload.token;
-      //   state.isLoggedIn = true;
-    },
-    [logOut.fulfilled](state) {
-      state.user = { name: null, email: null };
-      state.token = null;
-      state.isLoggedIn = false;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        console.log('register-action.payload: ', action.payload);
+        // state.user = action.payload.user;
+        // state.token = action.payload.token;
+        // state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        console.log('logIn-action.payload: ', action.payload);
+        // state.user = action.payload.user;
+        // state.token = action.payload.token;
+        // state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      });
   },
 });
 
