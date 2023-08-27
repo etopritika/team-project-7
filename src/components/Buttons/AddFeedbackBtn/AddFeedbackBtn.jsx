5094a99b227bb246c34a44f8667a2efb5fd6a2c2
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-08-27 16:20:36 +0300

    ave-header-start

--- src/components/Buttons/AddFeedbackBtn/AddFeedbackBtn.jsx
+++ src/components/Buttons/AddFeedbackBtn/AddFeedbackBtn.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import styles from './AddFeedbackBtn.module.css';
+
 export default function AddFeedbackBtn() {
-  return <button type="button">Feedback</button>;
+  return <button className={styles.feedbackBtn} type="button">Feedback</button>;
 }
