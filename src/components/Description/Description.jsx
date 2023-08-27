3ca382ba9b0e6d3708571ddc515d4aa5add9a6cf
VlShost <vlshost9386@gmail.com>
2023-08-27 18:05:49 +0300

    better styled reviews and small fixes

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,4 +1,5 @@
 import css from './Description.module.css';
+import '../../index.css';
 
 import calendarMob1x from '../../img/main-page/mobile/mobile-calendar.png';
 import calendarMob2x from '../../img/main-page/mobile/mobile-calendar@2x.png';
@@ -23,91 +24,94 @@ import allInOneDesk2x from '../../img/main-page/desktop/desktop-all-in-one@2x.pn
 
 export default function Description() {
   return (
-    <section className={css.descrSection}>
-      <ul className={css.descrList}>
-        <li className={css.descrItem}>
-          <div className={css.descrWrapper}>
-            <p className={css.descrNum}>1.</p>
-            <p className={css.descrAccent}>Calendar</p>
-            <p className={css.descrTitle}>view</p>
-            <p className={css.descrText}>
-              GooseTrack's Calendar view provides a comprehensive overview of
-              your schedule, displaying all your tasks, events, and appointments
-              in a visually appealing and intuitive layout.
-            </p>
-          </div>
-          <picture>
-            <source
-              media="(max-width: 767px)"
-              srcSet={`${calendarMob1x} 1x, ${calendarMob2x} 2x`}
-            />
-            <source
-              media="(min-width: 768px) and (max-width: 1439px)"
-              srcSet={`${calendarTabl1x} 1x, ${calendarTabl2x} 2x`}
-            />
-            <source
-              media="(min-width: 1440px)"
-              srcSet={`${calendarDesk1x} 1x, ${calendarDesk2x} 2x`}
-            />
-            <img src={calendarMob1x} alt="Calendar" />
-          </picture>
-        </li>
-        <li className={css.descrItem}>
-          <div className={css.descrWrapperSidebar}>
-            <p className={css.descrNum}>2.</p>
-            <p className={css.descrTitle}>SIDEBAR</p>
-            <p className={css.descrText}>
-              GooseTrack offers easy access to your account settings, calendar,
-              and filters. The "My Account" section allows you to manage your
-              profile information and preferences, while the calendar provides a
-              quick and convenient way to view your upcoming events and tasks.
-            </p>
-          </div>
-          <picture>
-            <source
-              media="(max-width: 767px)"
-              srcSet={`${sidebarMob1x} 1x, ${sidebarMob2x} 2x`}
-            />
-            <source
-              media="(min-width: 768px) and (max-width: 1439px)"
-              srcSet={`${sidebarTabl1x} 1x, ${sidebarTabl2x} 2x`}
-            />
-            <source
-              media="(min-width: 1440px)"
-              srcSet={`${sidebarDesk1x} 1x, ${sidebarDesk2x} 2x`}
-            />
-            <img src={sidebarMob1x} alt="Calendar" />
-          </picture>
-        </li>
-        <li className={css.descrItem}>
-          <div className={css.descrWrapper}>
-            <p className={css.descrNum}>3.</p>
-            <p className={css.descrAccent}>all in</p>
-            <p className={css.descrTitle}>one</p>
-            <p className={css.descrText}>
-              GooseTrack is an all-in-one productivity tool that helps you stay
-              on top of your tasks, events, and deadlines. Say goodbye to
-              scattered to-do lists and hello to streamlined productivity with
-              GooseTrack.
-            </p>
-          </div>
-          <picture>
-            <source
-              media="(max-width: 767px)"
-              srcSet={`${allInOneMob1x} 1x, ${allInOneMob2x} 2x`}
-            />
-            <source
-              media="(min-width: 768px) and (max-width: 1439px)"
-              srcSet={`${allInOneTabl1x} 1x, ${allInOneTabl2x} 2x`}
-            />
-            <source
-              media="(min-width: 1440px)"
-              srcSet={`${allInOneDesk1x} 1x, ${allInOneDesk2x} 2x`}
-            />
-            <img src={allInOneMob1x} alt="Calendar" />
-          </picture>
-        </li>
-      </ul>
-    </section>
+    <div className="container">
+      <section className={css.descrSection}>
+        <ul className={css.descrList}>
+          <li className={css.descrItem}>
+            <div className={css.descrWrapper}>
+              <p className={css.descrNum}>1.</p>
+              <p className={css.descrAccent}>Calendar</p>
+              <p className={css.descrTitle}>view</p>
+              <p className={css.descrText}>
+                GooseTrack's Calendar view provides a comprehensive overview of
+                your schedule, displaying all your tasks, events, and
+                appointments in a visually appealing and intuitive layout.
+              </p>
+            </div>
+            <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet={`${calendarMob1x} 1x, ${calendarMob2x} 2x`}
+              />
+              <source
+                media="(min-width: 768px) and (max-width: 1439px)"
+                srcSet={`${calendarTabl1x} 1x, ${calendarTabl2x} 2x`}
+              />
+              <source
+                media="(min-width: 1440px)"
+                srcSet={`${calendarDesk1x} 1x, ${calendarDesk2x} 2x`}
+              />
+              <img src={calendarMob1x} alt="Calendar" />
+            </picture>
+          </li>
+          <li className={css.descrItem}>
+            <div className={css.descrWrapperSidebar}>
+              <p className={css.descrNum}>2.</p>
+              <p className={css.descrTitle}>SIDEBAR</p>
+              <p className={css.descrText}>
+                GooseTrack offers easy access to your account settings,
+                calendar, and filters. The "My Account" section allows you to
+                manage your profile information and preferences, while the
+                calendar provides a quick and convenient way to view your
+                upcoming events and tasks.
+              </p>
+            </div>
+            <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet={`${sidebarMob1x} 1x, ${sidebarMob2x} 2x`}
+              />
+              <source
+                media="(min-width: 768px) and (max-width: 1439px)"
+                srcSet={`${sidebarTabl1x} 1x, ${sidebarTabl2x} 2x`}
+              />
+              <source
+                media="(min-width: 1440px)"
+                srcSet={`${sidebarDesk1x} 1x, ${sidebarDesk2x} 2x`}
+              />
+              <img src={sidebarMob1x} alt="Calendar" />
+            </picture>
+          </li>
+          <li className={css.descrItem}>
+            <div className={css.descrWrapper}>
+              <p className={css.descrNum}>3.</p>
+              <p className={css.descrAccent}>all in</p>
+              <p className={css.descrTitle}>one</p>
+              <p className={css.descrText}>
+                GooseTrack is an all-in-one productivity tool that helps you
+                stay on top of your tasks, events, and deadlines. Say goodbye to
+                scattered to-do lists and hello to streamlined productivity with
+                GooseTrack.
+              </p>
+            </div>
+            <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet={`${allInOneMob1x} 1x, ${allInOneMob2x} 2x`}
+              />
+              <source
+                media="(min-width: 768px) and (max-width: 1439px)"
+                srcSet={`${allInOneTabl1x} 1x, ${allInOneTabl2x} 2x`}
+              />
+              <source
+                media="(min-width: 1440px)"
+                srcSet={`${allInOneDesk1x} 1x, ${allInOneDesk2x} 2x`}
+              />
+              <img src={allInOneMob1x} alt="Calendar" />
+            </picture>
+          </li>
+        </ul>
+      </section>
+    </div>
   );
 }
--- src/components/ReviewsSlider/ReviewsSlider.jsx
+++ src/components/ReviewsSlider/ReviewsSlider.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation, Keyboard } from 'swiper/modules';
 
+import '../../index.css';
 import css from './ReviewsSlider.module.css';
+
+import icons from '../../img/icons.svg';
 import reviews from './reviews.json';
 
 import 'swiper/css';
@@ -10,40 +13,81 @@ import 'swiper/css/navigation';
 
 export default function ReviewsSlider() {
   return (
-    <section className={css.reviewsSection}>
-      <div className={css.wrapper}>
-        <h2 className={css.title}>Reviews</h2>
-        <Swiper
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-          navigation={{
-            prevEl: '#prevBtn',
-            nextEl: '#nextBtn',
-          }}
-          keyboard={{ enabled: true }}
-          modules={[Autoplay, Navigation, Keyboard]}
-        >
-          {reviews.map(({ name, comment }) => {
-            return (
-              <SwiperSlide key={name}>
-                <div className={css.reviewItem}>
-                  <p>{name}</p>
-                  <p>{comment}</p>
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        <div className={css.btnWrapper}>
-          <div id="prevBtn" className={css.btn}></div>
-          <div id="nextBtn" className={css.btn}></div>
+    <div className="container">
+      <section className={css.reviewsSection}>
+        <div className={css.wrapper}>
+          <h2 className={css.title}>Reviews</h2>
+          <Swiper
+            spaceBetween={30}
+            // centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            navigation={{
+              prevEl: '#prevBtn',
+              nextEl: '#nextBtn',
+            }}
+            breakpoints={{
+              1440: {
+                slidesPerView: 2,
+              },
+            }}
+            keyboard={{ enabled: true }}
+            modules={[Autoplay, Navigation, Keyboard]}
+          >
+            {reviews.map(({ avatar, name, comment, rating }) => {
+              return (
+                <SwiperSlide key={name}>
+                  <div className={css.reviewItem}>
+                    <div className={css.userInfo}>
+                      <div className={css.userAvatar}>
+                        {avatar ? (
+                          <img src={avatar} alt={name + 'avatar'} />
+                        ) : (
+                          <svg
+                            className={css.avatarPlaceholder}
+                            width="30px"
+                            height="30px"
+                          >
+                            <use href={icons + '#ph_user'}></use>
+                          </svg>
+                        )}
+                      </div>
+
+                      <div>
+                        <p className={css.userName}>{name}</p>
+                        <div className={css.userRating}>
+                          {Array.from({ length: 5 }, (_, idx) => (
+                            <svg key={idx} width="14px" height="14px">
+                              <use href={icons + '#star'}></use>
+                            </svg>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <p className={css.reviewComment}>{comment}</p>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+          <div className={css.btnWrapper}>
+            <button type="button" id="prevBtn" className={css.btn}>
+              <svg width="61px" height="61px">
+                <use href={icons + '#down-arrow-1-2'}></use>
+              </svg>
+            </button>
+
+            <button type="button" id="nextBtn" className={css.btn}>
+              <svg width="61px" height="61px">
+                <use href={icons + '#down-arrow-1-2'}></use>
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
