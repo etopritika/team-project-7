3e7801aae7ba473c032e52abe95ddb61dbbd99ba
olgachibis <olgachibis1968@gmail.com>
2023-08-28 14:21:36 +0300

    CalendarPage2

--- src/components/Calendar/CalendarToolbar/CalendarToolbar.jsx
+++ src/components/Calendar/CalendarToolbar/CalendarToolbar.jsx
@@ -1,3 +1,33 @@
-export default function CalendarToolbar(){
-    return(<></>)
-}
+// export default function CalendarToolbar(){
+//     return(<></>)
+// }
+import { useLocation } from 'react-router-dom';
+import { PeriodPaginator } from '../../PeriodPaginator/PeriodPaginator';
+import { PeriodTypeSelect } from '../../PeriodTypeSelect/PeriodTypeSelect';
+import styles from './CalendarToolbar.module.css';
+
+export default function CalendarToolbar() {
+  const location = useLocation();
+
+  const activePage = arr => {
+    let active;
+    arr.map(page => {
+      if (location.pathname.includes(page)) {
+        active = page;
+      }
+      return active;
+    });
+
+    return active;
+  };
+
+  const pages = ['month', 'day'];
+
+  return (
+    <div className={styles.wrapper}>
+      <PeriodPaginator activePage={activePage(pages)} />
+      <PeriodTypeSelect activePage={activePage(pages)} />
+    </div>
+  );
+};
+
--- src/components/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/PeriodPaginator/PeriodPaginator.jsx
@@ -1,3 +1,122 @@
-export default function PeriodPaginator(){
-    return(<></>)
-}
+// export default function PeriodPaginator(){
+//     return(<></>)
+// }
+import { format, subDays, addDays, addMonths, subMonths } from 'date-fns';
+import { useState } from 'react';
+import { ReactComponent as IconShevronLeft } from '../../img/icons.svg#shevron-left';
+import { ReactComponent as IconShevronRight } from '../../img/icons.svg#shevron-right';
+import styles from './PeriodPaginator.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useDate } from 'hooks/useDate';
+
+export const PeriodPaginator = ({ activePage }) => {
+  const [activeBtn, setActiveBtn] = useState('');
+
+  const navigate = useNavigate();
+  const urlDate = useDate();
+
+  const handleNextDay = () => {
+    setActiveBtn('next');
+
+    const date = addDays(urlDate, 1);
+    navigate(`/calendar/day/${format(date, 'ddMMMyyyy')}`);
+  };
+
+  const handlePrevDay = () => {
+    setActiveBtn('prev');
+
+    const date = subDays(urlDate, 1);
+    navigate(`/calendar/day/${format(date, 'ddMMMyyyy')}`);
+  };
+
+  const handleNextMonth = () => {
+    setActiveBtn('next');
+
+    const date = addMonths(urlDate, 1);
+    navigate(`/calendar/month/${format(date, 'MMMMyyyy')}`);
+  };
+
+  const handlePrevMonth = () => {
+    setActiveBtn('prev');
+
+    const date = subMonths(urlDate, 1);
+    navigate(`/calendar/month/${format(date, 'MMMMyyyy')}`);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      {activePage === 'month' && (
+        <>
+          <div className={styles.name_container}>
+            <h1 className={styles.name}>{format(urlDate, 'MMMM yyyy')}</h1>
+          </div>
+          <div className={styles.buttons_container}>
+            <button
+              className={`${styles.button} ${styles.button_left}`}
+              type="button"
+              onClick={handlePrevMonth}
+            >
+              <IconShevronLeft
+                className={
+                  activeBtn === 'prev'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+            <button
+              className={`${styles.button} ${styles.button_right}`}
+              type="button"
+              id={'2'}
+              onClick={handleNextMonth}
+            >
+              <IconShevronRight
+                className={
+                  activeBtn === 'next'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+          </div>
+        </>
+      )}
+
+      {activePage === 'day' && (
+        <>
+          <div className={styles.name_container}>
+            <h1 className={styles.name}>{format(urlDate, 'd MMM yyyy')}</h1>
+          </div>
+          <div className={styles.buttons_container}>
+            <button
+              className={`${styles.button} ${styles.button_left}`}
+              type="button"
+              onClick={handlePrevDay}
+            >
+              <IconShevronLeft
+                className={
+                  activeBtn === 'prev'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+            <button
+              className={`${styles.button} ${styles.button_right}`}
+              type="button"
+              onClick={handleNextDay}
+            >
+              <IconShevronRight
+                className={
+                  activeBtn === 'next'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
