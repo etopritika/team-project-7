ca8db42b6be6b6d733db8eccc845f4411da58bd0
Nikolai-desig <rinzleramad79@gmail.com>
2023-08-28 15:35:02 +0300

    Said bar end LogOut btn style

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import UserNav from './UserNavigation/UserNav';
 import LogoutBtn from 'components/Buttons/LogoutBtn/LogoutBtn';
 import styles from './SideBar.module.css';
+import SideBarLogo from './SideBarLogo/SideBarLogo';
 
 export default function SideBar() {
   return (
     <>
       <div className={styles.sidebarstyle}>
+      <SideBarLogo />
+      <p className={styles.userPanName}>User Panel</p>
         <UserNav />
         <LogoutBtn />
       </div>
