bb3ab0fded048359caf262ad76d7814d72ac2b37
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-08-28 13:24:49 +0300

    current-title

--- src/components/Header/HeaderTitle/HeaderTitle.jsx
+++ src/components/Header/HeaderTitle/HeaderTitle.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import styles from './HeaderTitle.module.css'
+import styles from './HeaderTitle.module.css';
 
-function HeaderTitle() {
-    return (
-        <div>
-            <h1 className={styles.headerTitle}>Current Title</h1>
-        </div>
-    )
+function HeaderTitle({ currentPage }) {
+  let pageTitle = 'Current Title';
+
+  switch (currentPage) {
+    case 'account':
+      pageTitle = 'Account';
+      break;
+    case 'calendar':
+      pageTitle = 'Calendar';
+      break;
+    case 'statistics':
+      pageTitle = 'Statistics';
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div>
+      <h1 className={styles.headerTitle}>{pageTitle}</h1>
+    </div>
+  );
 }
 
 export default HeaderTitle;
