ab177a3930afe982f2a6a9ca39a5b4179f8fd620
etopritika <dimaprytyka@gmail.com>
2023-08-28 11:48:45 +0300

    init hook fix

--- src/hooks/useUpdateUserInfo.js
+++ src/hooks/useUpdateUserInfo.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { selectUserInfo } from 'redux/auth/selectors';
+import { selectUserInfo } from '../redux/auth/selectors';
 
 export const useUpdateUserInfo = () => {
   const { name, email, phone, birthday, skype, userImgUrl } =
