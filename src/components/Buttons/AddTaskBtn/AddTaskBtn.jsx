d50874f4587aff894992afd8046ff32c633fc10b
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-08-31 21:51:07 +0300

    task-modal

--- src/components/Buttons/AddTaskBtn/AddTaskBtn.jsx
+++ src/components/Buttons/AddTaskBtn/AddTaskBtn.jsx
@@ -1,10 +1,10 @@
 import css from './AddTaskBtn.module.css';
 import icons from '../../../img/icons.svg';
 
-export default function AddTaskBtn() {
+export default function AddTaskBtn({ toggleModal }) {
   return (
     <>
-      <button className={css.btn}>
+      <button className={css.btn} onClick={toggleModal}>
         <svg className={css.btnIcon} width="24px" height="24px">
           <use href={icons + '#plus'}></use>
         </svg>
--- src/components/Tasks/TasksColumn/TasksColumn.jsx
+++ src/components/Tasks/TasksColumn/TasksColumn.jsx
@@ -1,13 +1,25 @@
 import ColumnHeadBar from '../../ColumnHeadBar/ColumnHeadBar';
 import ColumnTasksList from '../../ColumnTasksList/ColumnTasksList';
 import AddTaskBtn from '../../Buttons/AddTaskBtn/AddTaskBtn';
+import { useState } from 'react';
+import Modal from 'components/Modal/Modal';
 
 export default function TasksColumn({ title }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(prevState => !prevState);
+  };
   return (
     <>
       <ColumnHeadBar title={title} />
       <ColumnTasksList />
-      <AddTaskBtn />
+      <AddTaskBtn toggleModal={toggleModal} />
+      {isModalOpen && (
+        <Modal toggleModal={toggleModal}>
+          <div>Сюди треба розмістити таск-форм</div>
+        </Modal>
+      )}
     </>
   );
 }
