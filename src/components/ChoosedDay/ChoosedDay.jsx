e0477d4b1b6d607fd25fda1f641093af3a87a6cd
etopritika <dimaprytyka@gmail.com>
2023-08-31 19:04:32 +0300

    init fix

--- src/components/ChoosedDay/ChoosedDay.jsx
+++ src/components/ChoosedDay/ChoosedDay.jsx
@@ -1,11 +1,9 @@
-import CalendarToolbar from '../Calendar/CalendarToolbar/CalendarToolbar';
 import TasksColumnList from '../Tasks/TasksColumnList/TasksColumnList';
 
 
 export default function ChoosedDay() {
   return (
     <>
-      <CalendarToolbar />
       <TasksColumnList />
     </>
   );
