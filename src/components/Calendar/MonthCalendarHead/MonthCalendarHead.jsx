d020b342aec5526b5775bd1446ade4dff5f84d41
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-08-31 18:07:59 +0300

    calendar-deader-week

--- src/components/Calendar/MonthCalendarHead/MonthCalendarHead.jsx
+++ src/components/Calendar/MonthCalendarHead/MonthCalendarHead.jsx
@@ -1,3 +1,21 @@
-export default function MonthCalendarHead(){
-    return(<></>)
-}
+import React from 'react';
+import styles from './MonthCalendarHead.module.css';
+
+const MonthCalendarHead = () => {
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  return (
+    <ul className={styles['month-calendar-head']}>
+      {daysOfWeek.map((day, index) => (
+        <li key={index} className={styles['day-of-week']}>
+          <span className={styles['day-of-week-style']}>
+          {day}
+          </span>
+          
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MonthCalendarHead;
--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -1,9 +1,13 @@
-// import CalendarToolbar from '../Calendar/CalendarToolbar/CalendarToolbar';
+import React from 'react';
+// import CalendarTable from 'components/Calendar/CalendarTable/CalendarTable';
+import MonthCalendarHead from 'components/Calendar/MonthCalendarHead/MonthCalendarHead';
+import styles from './ChoosedMonth.module.css';
 
 export default function ChoosedMonth() {
   return (
-    <>
-      <span>ChoosedMonth</span>
-    </>
+    <div className={styles.month_container}>
+      <MonthCalendarHead />
+      {/* <CalendarTable /> */}
+    </div>
   );
 }
