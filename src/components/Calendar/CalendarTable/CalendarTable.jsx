43ec143f7dc5c123dbb878340a4da549926a9169
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-09-01 16:11:03 +0300

    calendar-fix-styles

--- src/components/Calendar/CalendarTable/CalendarTable.jsx
+++ src/components/Calendar/CalendarTable/CalendarTable.jsx
@@ -2,33 +2,51 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './CalendarTable.module.css';
 import Calendar from 'react-calendar';
-import { format } from 'date-fns';
-import { useParams } from "react-router-dom";
+import { format, isToday } from 'date-fns';
+import { useParams } from 'react-router-dom';
 
 export default function CalendarTable() {
-  const [/*selectedDate*/, setSelectedDate] = useState(null);
-  const {current} = useParams();
+  const [, /*selectedDate*/ setSelectedDate] = useState(null);
+  const { current } = useParams();
 
   const dateClickHandler = date => {
     setSelectedDate(date);
   };
 
+  const isCurrentDate = date => {
+    return isToday(date);
+  };
+
   const tileContent = ({ date }) => {
     const currentDay = format(date, 'ddMMMMyyyy');
-    return(<Link
-      to={`/user/calendar/day/${currentDay}`}
-      className={styles['calendar-link']}
-    >
-      <div className={styles['calendar-link-content']}>
-        <div className={styles['date-content']}>
-          <span>{date.getDate()}</span>
-        </div>
+    return (
+      <Link
+        to={`/user/calendar/day/${currentDay}`}
+        className={styles['calendar-link']}
+      >
+        <div className={styles['calendar-link-content']}>
+          <div className={styles['date-content']}>
+            <div
+              className={`${styles['date-content-container-number']} ${
+                isCurrentDate(date) ? styles['current-date'] : ''
+              }`}
+            >
+              <div
+                className={`${styles['date-content-number']} ${
+                  isCurrentDate(date) ? styles['current-date'] : ''
+                }`}
+              >
+                {date.getDate()}
+              </div>
+            </div>
+          </div>
 
-        <ul className={styles['calendar-day-content']}>
-          <li className={styles['todo-text']}>To Do</li>
-        </ul>
-      </div>
-    </Link>)
+          <ul className={styles['calendar-day-task']}>
+            <li className={styles['todo-items']}>To Do</li>
+          </ul>
+        </div>
+      </Link>
+    );
   };
   return (
     <div className={styles['calendar-container']}>
