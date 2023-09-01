8a0705d8bc2af39f5acc919525518e0803094f04
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-09-01 12:03:14 +0300

    Calendar

--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-// import CalendarTable from 'components/Calendar/CalendarTable/CalendarTable';
+
 import MonthCalendarHead from 'components/Calendar/MonthCalendarHead/MonthCalendarHead';
 import styles from './ChoosedMonth.module.css';
+import CalendarTable from 'components/Calendar/CalendarTable/CalendarTable';
 
 export default function ChoosedMonth() {
   return (
     <div className={styles.month_container}>
       <MonthCalendarHead />
-      {/* <CalendarTable /> */}
+      <CalendarTable />
     </div>
   );
 }
