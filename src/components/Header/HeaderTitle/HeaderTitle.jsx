7a06999972956fe863d0d2d8b8806e1dd11c35a6
olgachibis <olgachibis1968@gmail.com>
2023-09-02 22:11:18 +0300

    add Goose

--- src/components/Header/HeaderTitle/HeaderTitle.jsx
+++ src/components/Header/HeaderTitle/HeaderTitle.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './HeaderTitle.module.css';
+import gloryToTheMentor from '../../../img/glory_to_the_mentor.png';
 
 function HeaderTitle({ currentPage }) {
   let pageTitle = 'Current Title';
-
   if (currentPage.includes('account')) {
     pageTitle = 'Account';
   } else if (currentPage.includes('calendar')) {
@@ -12,9 +12,19 @@ function HeaderTitle({ currentPage }) {
     pageTitle = 'Statistics';
   }
 
-  return (
+  return pageTitle === 'Calendar' ? (
+    <div className={styles.container}>
+      <img src={gloryToTheMentor} alt="Слава ментору" className={styles.image} />
+      <div className={styles.text_container}>
+        <h1 className={styles.headerTitle}>{pageTitle}</h1>
+        <p className={styles.text}>
+          <span className={styles.textLetgo}>Let go</span> of the past and focus on the present!
+        </p>
+      </div>
+    </div>
+  ) : (
     <div>
-      <h1 className={styles.headerTitle}>{pageTitle}</h1>
+      <h1>{pageTitle}</h1>
     </div>
   );
 }
