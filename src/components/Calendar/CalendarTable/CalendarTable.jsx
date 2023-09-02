0d47ca0cbb5d97f6b0b8c1e44bb8fa6b0d5786cd
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-09-02 17:54:45 +0300

    calendar-current-month-tasks

--- src/components/Calendar/CalendarTable/CalendarTable.jsx
+++ src/components/Calendar/CalendarTable/CalendarTable.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './CalendarTable.module.css';
 import Calendar from 'react-calendar';
-import { format, isToday } from 'date-fns';
+import { format, isToday, addMonths, subMonths } from 'date-fns';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function CalendarTable() {
-  const [/*selectedDate*/, setSelectedDate] = useState(null);
+  const [, /*selectedDate*/ setSelectedDate] = useState(null);
   const { current } = useParams();
+
+  const tasks = useSelector(state => state.tasks.items);
 
   const dateClickHandler = date => {
     setSelectedDate(date);
@@ -17,12 +20,46 @@ export default function CalendarTable() {
     return isToday(date);
   };
 
- const tileContent = ({ date }) => {
+  const isSameMonth = (date, targetMonth) => {
+    return (
+      date.getMonth() === targetMonth.getMonth() &&
+      date.getFullYear() === targetMonth.getFullYear()
+    );
+  };
+
+  const getTileContentStyles = date => {
+    const currentMonth = current ? new Date(current) : new Date();
+    const prevMonth = subMonths(currentMonth, 1);
+    const nextMonth = addMonths(currentMonth, 1);
+
+    if (isSameMonth(date, currentMonth)) {
+      return '';
+    } else if (isSameMonth(date, prevMonth) || isSameMonth(date, nextMonth)) {
+      return styles['other-month'];
+    } else {
+      return '';
+    }
+  };
+
+  const getTasksForDate = date => {
+    // Фільтруємо завдання, що належать до вказаної дати
+    return tasks.filter(task => {
+      const taskDate = new Date(task.date); // date дата таски
+      return (
+        taskDate.getDate() === date.getDate() &&
+        taskDate.getMonth() === date.getMonth() &&
+        taskDate.getFullYear() === date.getFullYear()
+      );
+    });
+  };
+
+  const tileContent = ({ date }) => {
     const currentDay = format(date, 'ddMMMMyyyy');
+    const dayTasks = getTasksForDate(date);
     return (
       <Link
         to={`/user/calendar/day/${currentDay}`}
-        className={styles['calendar-link']}
+        className={`${styles['calendar-link']} ${getTileContentStyles(date)}`}
       >
         <div className={styles['calendar-link-content']}>
           <div className={styles['date-content']}>
@@ -42,19 +79,24 @@ export default function CalendarTable() {
           </div>
 
           <ul className={styles['calendar-day-task']}>
-            <li className={styles['todo-items']}>To Do</li>
+            {/* Відображаємо завдання для цього дня */}
+            {dayTasks.map(task => (
+              <li key={task.id} className={styles['todo-items']}>
+                {task.title}
+              </li>
+            ))}
           </ul>
         </div>
       </Link>
     );
   };
+
   return (
     <div className={styles['calendar-container']}>
       <div className={styles['calendar-wrapper']}>
         <Calendar
           value={current}
           onClickDay={dateClickHandler}
-          tileClassName={() => styles['custom-tile']}
           tileContent={tileContent}
           showNavigation={false}
           showWeekNumbers={false}
