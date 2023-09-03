a87a6b5e25d44a1e549ebb6c17f7481836dd2b26
olgachibis <olgachibis1968@gmail.com>
2023-09-03 16:41:52 +0300

    fix condition

--- src/components/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/PeriodPaginator/PeriodPaginator.jsx
@@ -135,6 +135,41 @@ export const PeriodPaginator = ({ activePage }) => {
           </div>
         </>
       )}
+      {activePage === '/statistics/day' && (
+        <>
+          <div className={styles.name_container_day}>
+            <h1 className={styles.name}>{format(visibleDate, 'd MMM yyyy')}</h1>
+          </div>
+          <div className={styles.buttons_container}>
+            <button
+              className={`${styles.button} ${styles.button_left}`}
+              type="button"
+              onClick={handlePrevDay}
+            >
+              <FiChevronLeft
+                className={
+                  activeBtn === 'prev'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+            <button
+              className={`${styles.button} ${styles.button_right}`}
+              type="button"
+              onClick={handleNextDay}
+            >
+              <FiChevronRight
+                className={
+                  activeBtn === 'next'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
