9a7b679d0c5fcb6d70c6e9b10c653df95e9e109f
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-09-03 22:24:04 +0300

    tasks-add

--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MonthCalendarHead from 'components/Calendar/MonthCalendarHead/MonthCalendarHead';
 import styles from './ChoosedMonth.module.css';
 import CalendarTable from 'components/Calendar/CalendarTable/CalendarTable';
+import { useDispatch } from 'react-redux';
+import { fetchTasks } from 'redux/tasks/taskOperations';
 
 export default function ChoosedMonth() {
+
+  const [tasks, setTasks] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchTasksData = async () => {
+      try {
+        const fetchedTasks = await dispatch(fetchTasks());
+        setTasks(fetchedTasks.payload);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+
+    fetchTasksData();
+  }, [dispatch]);
   return (
     <div className={styles.month_container}>
       <MonthCalendarHead />
-      <CalendarTable />
+      <CalendarTable tasks={tasks} />
     </div>
   );
 }
