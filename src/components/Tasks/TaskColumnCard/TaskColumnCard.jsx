52c71d98b7426976c07a4dbe7f53e12a2c2adf9f
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-09-04 12:33:16 +0300

    task-card

--- src/components/Tasks/TaskColumnCard/TaskColumnCard.jsx
+++ src/components/Tasks/TaskColumnCard/TaskColumnCard.jsx
@@ -1,7 +1,23 @@
+import { SlPencil } from 'react-icons/sl';
+import styles from './TaskColumnCard.module.css';
+
 export default function TaskColumnCard({ task }) {
   return (
-    <>
-      <p>{task.title}</p>
-    </>
+    <div className={styles.containerCard}>
+      <h3 className={styles.titleCard}>{task.title}</h3>
+      <div className={styles.contentContainer}>
+        <div className={styles.avatarContainer}>
+          <img src={task.avatar} alt="User Avatar" className={styles.avatar} />
+        </div>
+        <div className={styles.priorityContainer}>{task.priority}</div>
+        <div className={styles.btnContainer}>
+          <button type="button" className={styles.btn}></button>
+          <button type="button" className={styles.btn}>
+            <SlPencil className={styles.btnIcon} />
+          </button>
+          <button type="button" className={styles.btn}></button>
+        </div>
+      </div>
+    </div>
   );
 }
