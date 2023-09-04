8e6c6eb80772b07af21984047334c0724ee7304c
Anton Alokhin <anton.alyokhin1997@gmail.com>
2023-09-04 16:14:22 +0300

    add TaskColumnCard

--- src/components/Tasks/TaskColumnCard/TaskColumnCard.jsx
+++ src/components/Tasks/TaskColumnCard/TaskColumnCard.jsx
@@ -1,23 +1,41 @@
-import { SlPencil } from 'react-icons/sl';
-import styles from './TaskColumnCard.module.css';
-
-export default function TaskColumnCard({ task }) {
-  return (
-    <div className={styles.containerCard}>
-      <h3 className={styles.titleCard}>{task.title}</h3>
-      <div className={styles.contentContainer}>
-        <div className={styles.avatarContainer}>
-          <img src={task.avatar} alt="User Avatar" className={styles.avatar} />
-        </div>
-        <div className={styles.priorityContainer}>{task.priority}</div>
-        <div className={styles.btnContainer}>
-          <button type="button" className={styles.btn}></button>
-          <button type="button" className={styles.btn}>
-            <SlPencil className={styles.btnIcon} />
-          </button>
-          <button type="button" className={styles.btn}></button>
-        </div>
-      </div>
-    </div>
-  );
-}
+import { useAuth } from 'hooks/useAuth'; 
+import { TaskToolbar } from '../TaskToolbar/TaskToolbar'; 
+import css from './TaskColumnCard.module.css'; 
+ 
+export const TaskColumnCard = ({task}) => { 
+  function getInitials(name) { 
+    if (name) { 
+      const initials = name 
+        .split(' ') 
+        .map(word => word.charAt(0)) 
+        .join('') 
+        .toUpperCase(); 
+      return initials; 
+    } else { 
+      return name; 
+    } 
+  } 
+ 
+  const { name, userImgUrl } = useAuth(); 
+ 
+  const displayName = userImgUrl ? ( 
+    <img src={userImgUrl} alt="UserPicture" /> 
+  ) : ( 
+    <p className="initials">{getInitials(name)}</p> 
+  ); 
+ 
+  return ( 
+    <div className={css.taskCard}> 
+      <h3 className={css.cardTitle}>{task.title}</h3> 
+      <div className={css.cardInfo}> 
+        <div className={css.styledInfo}> 
+          <div className={css.userLogo}> 
+            {displayName} 
+          </div> 
+          <p className={css.priorityTitle}>{task.priority}</p> 
+        </div> 
+        <TaskToolbar /> 
+      </div> 
+    </div> 
+  ); 
+};
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,18 +1,20 @@
-import { useSelector } from 'react-redux';
-import {
-  selectUser,
-  selectIsLoggedIn,
-  selectIsRefreshing,
-} from '../redux/auth/selectors';
-
-export const useAuth = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const user = useSelector(selectUser);
-  const isRefreshing = useSelector(selectIsRefreshing);
-
-  return {
-    isLoggedIn,
-    user,
-    isRefreshing,
-  };
+import { useSelector } from 'react-redux'; 
+import { 
+  selectUser, 
+  selectIsLoggedIn, 
+  selectIsRefreshing, 
+} from '../redux/Auth/selectors'; 
+ 
+export const useAuth = () => { 
+  const isLoggedIn = useSelector(selectIsLoggedIn); 
+  const { name, userImgUrl } = 
+    useSelector(selectUser); 
+  const isRefreshing = useSelector(selectIsRefreshing); 
+ 
+  return { 
+    isLoggedIn, 
+    name, 
+    userImgUrl, 
+    isRefreshing, 
+  }; 
 };
