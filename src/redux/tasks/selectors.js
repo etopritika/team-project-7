afd66794153461a765a7a8a51405d24551768b85
Anton Alokhin <anton.alyokhin1997@gmail.com>
2023-09-04 19:19:20 +0300

    add TaskToolbar

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -3,3 +3,5 @@ export const selectTasks = state => state.tasks.items;
 export const selectIsLoading = state => state.tasks.isLoading;
 
 export const selectError = state => state.tasks.error;
+
+export const selectDate = state => state.date.currentDate;
