e30bf1c1473420465741f4177e2d8436c631da00
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-09-06 19:32:44 +0300

    fix-header

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,9 +1,15 @@
 import { useAuth } from '../../hooks/useAuth';
 import { useMemo } from 'react';
 import css from './UserInfo.module.css';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function UserInfo(){
   const { user } = useAuth();
+
+  const location = useLocation();
+  function isActive(pathname) {
+    return location.pathname.includes(pathname);
+  }
   
   const {name, avatarURL} = user;
 
@@ -30,10 +36,12 @@ export default function UserInfo(){
   //   </div>
   // );
   return (
+    <Link to="/user/account" className={`${css.navAvatar} ${isActive('/user/account') ? css.active : ''}`}>
     <div className={css.wrapper}>
       <p className={css.userName}>{memoizedName}</p>
       {/* <div className={css.userPicture }>{displayName}</div> */}
       <img className={css.userPicture } src={memoizedAvatarURL} alt="UserPicture" />
     </div>
+    </Link>
   );
 };
